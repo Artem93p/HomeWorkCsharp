b5bc2ae08fff7cffd980fa9e1feddfb85a1ef2e9
ArtemP <artem93.01@mail.ru>
2022-12-11 18:24:57 +0300

    4 работа

---
+++ 25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+
+int Promt(string str)
+{
+    Console.Write(str);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int Exponentiation(int A, int B)
+{
+    int A1 = A;
+    for (int i = 1; i < B; i++)
+    {
+        int result = A1 * A;
+        A1 = result;
+    }
+    return A1;
+}
+
+int A = Promt("Введите число А: ");
+int B = Promt("Введите число B: ");
+
+int result = Exponentiation(A, B);
+Console.WriteLine(result);
---
+++ 27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+Console.Clear();
+
+int Promt(string str)
+{
+    Console.Write(str);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int Sum(int number)
+{
+    int rezult = 0;
+    while (number > 0)
+    {
+        int ost = number % 10;
+        int sumOst = rezult + ost;
+        number = number / 10;
+        rezult = sumOst;
+    }
+    return rezult;
+}
+
+int number = Promt("Введите число: ");
+int result = Sum(number);
+Console.Write(result);
