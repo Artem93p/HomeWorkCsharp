2ef142060f584ec8741ad8f77f1ff0ce91f0ea67
Artem93p <108241502+Artem93p@users.noreply.github.com>
2022-12-12 23:45:52 +0300

    Домашняя работа №5

---
+++ Program34.cs
@@ -0,0 +1,34 @@
+﻿// /Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int SIZE = 20;
+int LEFT_RANGE = 99;
+int RIGHT_RANGE = 1000;
+
+int[] array = new int[SIZE];
+Random rand = new Random();
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+array = FillArray(SIZE, LEFT_RANGE, RIGHT_RANGE); // обращаемся к методу.
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine("[" + string.Join(",", array) + "]");
+System.Console.WriteLine("Четных чисел " + count);
---
+++ Program38.cs
@@ -0,0 +1,38 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int SIZE = 10;
+
+double[] array = new double[SIZE];
+Random rand = new Random();
+
+double[] FillArray(int size)
+{
+    Random rand = new Random();
+    double[] arr = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.NextDouble() * 100;
+    }
+    return arr;
+}
+array = FillArray(SIZE);
+
+double min = array[0];
+double max = array[0];
+for (int i = 0; i < SIZE; i++)
+{
+    if (min >= array[i])
+    {
+        min = array[i];
+    }
+    if (max <= array[i])
+    {
+        max = array[i];
+    }
+}
+double result = max - min;
+
+System.Console.WriteLine("[" + string.Join("; ", array) + "]");
+System.Console.WriteLine("Разница между минимальным и максимальным числом массива = " + Math.Round(result, 3));
